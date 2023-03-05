715f11eabd96325cf91825cda8530b41a1eca91c
Linuxcode1 <64568214+Linuxcode1@users.noreply.github.com>
2023-03-05 22:45:17 +0200

    Update Bahaa_FeedsFinder.sh

--- Bahaa_FeedsFinder.sh
+++ Bahaa_FeedsFinder.sh
@@ -8,8 +8,7 @@ echo "#########################################################"
 echo "#               FeedsFinder PY3 Mod BY                  #"
 echo "#                   BAhaa LinuXcode                     #"
 echo "#########################################################"
-echo "#                Restart Enigma2 GUI                    #"
-echo "#########################################################"
+
 
 rm -rf /usr/lib/enigma2/python/Plugins/Extensions/FeedsFinder > /dev/null 2>&1
 if [ "$PYTHON_VERSION" == 3.9.9 -o "$PYTHON_VERSION" == 3.9.7 ]; then
@@ -22,14 +21,14 @@ rm -f /tmp/FeedsFinder-Mod-LinuXcode.tar.gz
 elif [ "$PYTHON_VERSION" == 3.10.4 ]; then
 echo ":You have $PYTHON_VERSION image ..."
 
-wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=1uWWj8SGbt-AmqFM0bMlWmCPYepQ7cnn-' -O /tmp/FeedsFinder-Mod-LinuXcode.tar.gz;
+wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=1uWWj8SGbt-AmqFM0bMlWmCPYepQ7cnn-' -O /tmp/FeedsFinder-Mod-LinuXcode.tar.gz
 tar -xzf /tmp/FeedsFinder-Mod-LinuXcode.tar.gz -C /
 rm -f /tmp/FeedsFinder-Mod-LinuXcode.tar.gz
 
 
 elif [ "$PYTHON_VERSION" == 3.11.0 ]; then
 echo ":You have $PYTHON_VERSION image ..."
-wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=https://drive.google.com/file/d/17VFTl3fsPUpDqz-7xtDiCzrtd48Alyg7/view?usp=share_link' -O /tmp/FeedsFinder-Mod-LinuXcode.tar.gz;
+wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=17VFTl3fsPUpDqz-7xtDiCzrtd48Alyg7' -O /tmp/FeedsFinder-Mod-LinuXcode.tar.gz
 tar -xzf /tmp/FeedsFinder-Mod-LinuXcode.tar.gz -C /
 rm -f /tmp/FeedsFinder-Mod-LinuXcode.tar.gz
 
