8bec4d99ebd24928f23ac0b9f1bd4c946b4864f1
Linuxcode1 <64568214+Linuxcode1@users.noreply.github.com>
2023-03-05 22:33:55 +0200

    Add files via upload

---
+++ Bahaa_FeedsFinder.sh
@@ -0,0 +1,53 @@
+VERSION=3.11
+PLUGIN_PATH='/usr/lib/enigma2/python/Plugins/Extensions/MultiStalker'
+PYTHON_VERSION=$(python -c"import platform; print(platform.python_version())")
+
+
+
+echo "#########################################################"
+echo "#               FeedsFinder PY3 Mod BY                  #"
+echo "#                   BAhaa LinuXcode                     #"
+echo "#########################################################"
+echo "#                Restart Enigma2 GUI                    #"
+echo "#########################################################"
+
+rm -rf /usr/lib/enigma2/python/Plugins/Extensions/FeedsFinder > /dev/null 2>&1
+if [ "$PYTHON_VERSION" == 3.9.9 -o "$PYTHON_VERSION" == 3.9.7 ]; then
+echo ":You have $PYTHON_VERSION image ..."
+wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=11ps5Yy2tQ7vh0SElhaz7ZfJVEtX43cSe' -O /tmp/FeedsFinder-Mod-LinuXcode.tar.gz;
+tar -xzf /tmp/FeedsFinder-Mod-LinuXcode.tar.gz -C /
+rm -f /tmp/FeedsFinder-Mod-LinuXcode.tar.gz
+
+
+elif [ "$PYTHON_VERSION" == 3.10.4 ]; then
+echo ":You have $PYTHON_VERSION image ..."
+
+wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=1uWWj8SGbt-AmqFM0bMlWmCPYepQ7cnn-' -O /tmp/FeedsFinder-Mod-LinuXcode.tar.gz;
+tar -xzf /tmp/FeedsFinder-Mod-LinuXcode.tar.gz -C /
+rm -f /tmp/FeedsFinder-Mod-LinuXcode.tar.gz
+
+
+elif [ "$PYTHON_VERSION" == 3.11.0 ]; then
+echo ":You have $PYTHON_VERSION image ..."
+wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=https://drive.google.com/file/d/17VFTl3fsPUpDqz-7xtDiCzrtd48Alyg7/view?usp=share_link' -O /tmp/FeedsFinder-Mod-LinuXcode.tar.gz;
+tar -xzf /tmp/FeedsFinder-Mod-LinuXcode.tar.gz -C /
+rm -f /tmp/FeedsFinder-Mod-LinuXcode.tar.gz
+
+else
+exit 1
+fi
+
+echo ""
+echo "#########################################################"
+echo "#     FeedsFinder $VERSION INSTALLED SUCCESSFULLY       #"
+echo "#                Mod BY BAhaa LinuXcode                 #"
+echo "#########################################################"
+echo "#                Restart Enigma2 GUI                    #"
+echo "#########################################################"
+sleep 2
+if [ $OS = 'DreamOS' ]; then
+systemctl restart enigma2
+else
+killall -9 enigma2
+fi
+exit 0
